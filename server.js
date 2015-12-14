3ce2bb2224b73fc40a5711dacd7e4e5c154dd3fc
Léo LE BRAS <leo.lebrasf@gmail.com>
2015-12-14 15:09:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express'),
-	app = express();
+import express from 'express';
 
-var port = process.env.PORT || 8080;
-var io = require('socket.io').listen(app.listen(port));
+const app = express();
+const port = process.env.PORT || 8080;
+const io = require('socket.io').listen(app.listen(port));
 
 app.use(express.static(__dirname + '/public/__build__/'));
 
 app.listen(3000, () => {
-  console.log('Server running at localhost:3000/'.underline);
+  console.log('Server running at localhost:3000/');
 });
