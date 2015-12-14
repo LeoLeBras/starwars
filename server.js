5714aa1729faf7b0d188fb16e2ee5d683c4f29c4
Léo LE BRAS <leo.lebrasf@gmail.com>
2015-12-14 15:18:34 +0100

    Fix port on server.js

--- server.js
+++ server.js
@@ -6,6 +6,4 @@ const io = require('socket.io').listen(app.listen(port));
 
 app.use(express.static(__dirname + '/public/__build__/'));
 
-app.listen(3000, () => {
-  console.log('Server running at localhost:3000/');
-});
+console.log('Server running at localhost:' + port + '/');
