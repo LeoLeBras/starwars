b9f362ed3e415be9aa9746cd00ffc2a1ed026f98
Léo LE BRAS <leo.lebrasf@gmail.com>
2015-12-16 13:49:03 +0100

    Add style on desktop-home.vue

--- public/src/js/components/desktop-home.vue
+++ public/src/js/components/desktop-home.vue
@@ -1,6 +1,24 @@
 <template>
     <div class="container">
-        <h1>Navigation</h1>
-        <a v-link="'phone'">Phone</a>
+        <div class="wrapper">
+            <h1>Navigation</h1>
+            <a v-link="'phone'">Phone</a>
+        </div>
     </div>
 </template>
+
+<style scoped>
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        min-height: 100vh;
+    }
+
+    .wrapper {
+        margin: auto;
+        text-align: center;
+    }
+
+</style>
