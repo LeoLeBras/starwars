0ca73aae7e1fa99804f32f4fa6247e20cc144b83
Léo LE BRAS <leo.lebrasf@gmail.com>
2015-12-16 18:55:16 +0100

    Fix link on desktop-home.vue

--- public/src/js/components/desktop-home.vue
+++ public/src/js/components/desktop-home.vue
@@ -7,7 +7,7 @@
             <p class="mode">
                 Choose your navigation mode.
             </p>
-            <a v-link="phone" class="phone">
+            <a v-link="'phone'" class="phone">
                 <svg width="64px" height="124px" viewBox="0 0 64 124" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
                     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
                         <g id="Home-" sketch:type="MSArtboardGroup" transform="translate(-502.000000, -800.000000)">
