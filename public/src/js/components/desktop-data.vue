6eec790a80dd4f3f1891898014deb0e9fb656237
Alex Berneau <haldir.alex@gmail.com>
2015-12-17 18:38:49 +0100

    Desktop-home-vue v2 + animations

--- public/src/js/components/desktop-data.vue
+++ public/src/js/components/desktop-data.vue
@@ -4,14 +4,56 @@
         </div>
 
         <div class="wrapper">
-            <div class="planet-tatooine">
 
+            <div class="planet-tatooine"></div>
+            <div class="round-1"></div>
+            <div class="round-2"></div>
+            <div class="round-3"></div>
+            <div class="data-slider">
+            	<div class='alfa' title='one'>
+                	<div class='bravo' title='two'>
+                    	<textarea readonly cols='0' rows='0' class='charly'></textarea>
+                	</div>
+            	</div>
             </div>
-            <div class='alfa' title='one'>
-                <div class='bravo' title='two'>
-                    <textarea readonly cols='0' rows='0' class='charly'></textarea>
-                </div>
+            <div class="data-resident">
+            	Population
+            	<br>200 000
             </div>
+            <div class="data-chapter">
+				<svg width="232px" height="232px" viewBox="0 0 232 232" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+				    <defs>
+				        <radialGradient cx="50%" cy="50%" fx="50%" fy="50%" r="55.3668478%" id="radialGradient-1">
+				            <stop stop-color="#000000" stop-opacity="0.0124547101" offset="0%"></stop>
+				            <stop stop-color="#921111" offset="100%"></stop>
+				        </radialGradient>
+				    </defs>
+				    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+				        <g id="Data-Planet-Tatooine" sketch:type="MSArtboardGroup" transform="translate(-1006.000000, -66.000000)">
+				            <g id="####" sketch:type="MSLayerGroup" transform="translate(1007.000000, 67.000000)">
+				                <g id="Round" stroke="#FEFEFE" sketch:type="MSShapeGroup">
+				                    <circle id="Oval-50756" fill="url(#radialGradient-1)" cx="115" cy="115" r="115"></circle>
+				                    <path d="M115.5,0.5 L115.5,114.5 M224.395971,79.6175541 L115.975528,114.845492 M182.801411,207.632446 L115.793893,115.404508 M48.1985886,207.632446 L115.206107,115.404508 M6.60402888,79.6175541 L115.024472,114.845492" id="Line" stroke-linecap="square"></path>
+				                </g>
+				                <g id="Numbers" transform="translate(44.000000, 46.000000)" fill="#FEFEFE" sketch:type="MSShapeGroup">
+				                    <path d="M101.603626,4.69588665 L108.603626,4.69588665 L108.603626,5.69588665 C108.07029,5.69588665 107.616961,5.86921825 107.243626,6.21588665 C106.870291,6.56255505 106.65696,7.00255065 106.603626,7.53588665 L106.603626,33.8558866 C106.65696,34.3625558 106.856958,34.7825516 107.203626,35.1158866 C107.550294,35.4492216 108.016956,35.642553 108.603626,35.6958866 L108.603626,36.6958866 L101.603626,36.6958866 L101.603626,35.6958866 C102.163629,35.6958866 102.636957,35.5025552 103.023626,35.1158866 C103.410295,34.729218 103.603626,34.2558894 103.603626,33.6958866 L103.603626,7.53588665 C103.550292,7.05588425 103.343628,6.64255505 102.983626,6.29588665 C102.623624,5.94921825 102.163629,5.74922025 101.603626,5.69588665 L101.603626,4.69588665 Z" id="1" transform="translate(106.103626, 20.695887) rotate(35.000000) translate(-106.103626, -20.695887) "></path>
+				                    <path d="M54,110 L61,110 L61,111 C60.466664,111 60.0133352,111.173332 59.64,111.52 C59.2666648,111.866668 59.0533336,112.306664 59,112.84 L59,139.16 C59.0533336,139.666669 59.2533316,140.086665 59.6,140.42 C59.9466684,140.753335 60.4133304,140.946666 61,141 L61,142 L54,142 L54,141 C54.5600028,141 55.0333314,140.806669 55.42,140.42 C55.8066686,140.033331 56,139.560003 56,139 L56,112.84 C55.9466664,112.359998 55.7400018,111.946668 55.38,111.6 C55.0199982,111.253332 54.5600028,111.053334 54,111 L54,110 Z M77,110 L82,110 L82,111 C81.6533316,111 81.346668,111.126665 81.08,111.38 C80.813332,111.633335 80.5333348,112.173329 80.24,113 L73,142 L64.84,113 C64.5199984,112.173329 64.226668,111.640001 63.96,111.4 C63.693332,111.159999 63.3733352,111.026667 63,111 L63,110 L69,110 L69,111 C68.4933308,111.053334 68.1600008,111.219999 68,111.5 C67.8399992,111.780001 67.7466668,112.146664 67.72,112.6 L73.32,132.64 L78.24,113 C78.24,112.466664 78.173334,112.013335 78.04,111.64 C77.906666,111.266665 77.5600028,111.053334 77,111 L77,110 Z" id="IV" transform="translate(69.000000, 126.000000) rotate(180.000000) translate(-69.000000, -126.000000) "></path>
+				                    <path d="M122.696034,74.6724491 L129.696034,74.6724491 L129.696034,75.6724491 C129.162698,75.6724491 128.709369,75.8457807 128.336034,76.1924491 C127.962699,76.5391175 127.749368,76.9791131 127.696034,77.5124491 L127.696034,103.832449 C127.749368,104.339118 127.949366,104.759114 128.296034,105.092449 C128.642702,105.425784 129.109364,105.619116 129.696034,105.672449 L129.696034,106.672449 L122.696034,106.672449 L122.696034,105.672449 C123.256037,105.672449 123.729365,105.479118 124.116034,105.092449 C124.502703,104.705781 124.696034,104.232452 124.696034,103.672449 L124.696034,77.5124491 C124.6427,77.0324467 124.436036,76.6191175 124.076034,76.2724491 C123.716032,75.9257807 123.256037,75.7257827 122.696034,75.6724491 L122.696034,74.6724491 Z M128.696034,74.6724491 L135.696034,74.6724491 L135.696034,75.6724491 C135.162698,75.6724491 134.709369,75.8457807 134.336034,76.1924491 C133.962699,76.5391175 133.749368,76.9791131 133.696034,77.5124491 L133.696034,103.832449 C133.749368,104.339118 133.949366,104.759114 134.296034,105.092449 C134.642702,105.425784 135.109364,105.619116 135.696034,105.672449 L135.696034,106.672449 L128.696034,106.672449 L128.696034,105.672449 C129.256037,105.672449 129.729365,105.479118 130.116034,105.092449 C130.502703,104.705781 130.696034,104.232452 130.696034,103.672449 L130.696034,77.5124491 C130.6427,77.0324467 130.436036,76.6191175 130.076034,76.2724491 C129.716032,75.9257807 129.256037,75.7257827 128.696034,75.6724491 L128.696034,74.6724491 Z M134.696034,74.6724491 L141.696034,74.6724491 L141.696034,75.6724491 C141.162698,75.6724491 140.709369,75.8457807 140.336034,76.1924491 C139.962699,76.5391175 139.749368,76.9791131 139.696034,77.5124491 L139.696034,103.832449 C139.749368,104.339118 139.949366,104.759114 140.296034,105.092449 C140.642702,105.425784 141.109364,105.619116 141.696034,105.672449 L141.696034,106.672449 L134.696034,106.672449 L134.696034,105.672449 C135.256037,105.672449 135.729365,105.479118 136.116034,105.092449 C136.502703,104.705781 136.696034,104.232452 136.696034,103.672449 L136.696034,77.5124491 C136.6427,77.0324467 136.436036,76.6191175 136.076034,76.2724491 C135.716032,75.9257807 135.256037,75.7257827 134.696034,75.6724491 L134.696034,74.6724491 Z" id="3" transform="translate(133.196034, 90.672449) rotate(-73.000000) translate(-133.196034, -90.672449) "></path>
+				                    <path d="M36.5583267,5.22182984 L41.5583267,5.22182984 L41.5583267,6.22182984 C41.2116583,6.22182984 40.9049947,6.34849524 40.6383267,6.60182984 C40.3716587,6.85516444 40.0916615,7.39515904 39.7983267,8.22182984 L32.5583267,37.2218298 L24.3983267,8.22182984 C24.0783251,7.39515904 23.7849947,6.86183104 23.5183267,6.62182984 C23.2516587,6.38182864 22.9316619,6.24849664 22.5583267,6.22182984 L22.5583267,5.22182984 L28.5583267,5.22182984 L28.5583267,6.22182984 C28.0516575,6.27516344 27.7183275,6.44182844 27.5583267,6.72182984 C27.3983259,7.00183124 27.3049935,7.36849424 27.2783267,7.82182984 L32.8783267,27.8618298 L37.7983267,8.22182984 C37.7983267,7.68849384 37.7316607,7.23516504 37.5983267,6.86182984 C37.4649927,6.48849464 37.1183295,6.27516344 36.5583267,6.22182984 L36.5583267,5.22182984 Z M40.5583267,5.22182984 L47.5583267,5.22182984 L47.5583267,6.22182984 C47.0249907,6.22182984 46.5716619,6.39516144 46.1983267,6.74182984 C45.8249915,7.08849824 45.6116603,7.52849384 45.5583267,8.06182984 L45.5583267,34.3818298 C45.6116603,34.888499 45.8116583,35.3084948 46.1583267,35.6418298 C46.5049951,35.9751648 46.9716571,36.1684962 47.5583267,36.2218298 L47.5583267,37.2218298 L40.5583267,37.2218298 L40.5583267,36.2218298 C41.1183295,36.2218298 41.5916581,36.0284984 41.9783267,35.6418298 C42.3649953,35.2551612 42.5583267,34.7818326 42.5583267,34.2218298 L42.5583267,8.06182984 C42.5049931,7.58182744 42.2983285,7.16849824 41.9383267,6.82182984 C41.5783249,6.47516144 41.1183295,6.27516344 40.5583267,6.22182984 L40.5583267,5.22182984 Z" id="6" transform="translate(36.058327, 21.221830) rotate(-35.000000) translate(-36.058327, -21.221830) "></path>
+				                    <path d="M22.9337042,70.6724491 L27.9337042,70.6724491 L27.9337042,71.6724491 C27.5870358,71.6724491 27.2803722,71.7991145 27.0137042,72.0524491 C26.7470362,72.3057837 26.467039,72.8457783 26.1737042,73.6724491 L18.9337042,102.672449 L10.7737042,73.6724491 C10.4537026,72.8457783 10.1603722,72.3124503 9.89370418,72.0724491 C9.62703618,71.8324479 9.30703938,71.6991159 8.93370418,71.6724491 L8.93370418,70.6724491 L14.9337042,70.6724491 L14.9337042,71.6724491 C14.427035,71.7257827 14.093705,71.8924477 13.9337042,72.1724491 C13.7737034,72.4524505 13.680371,72.8191135 13.6537042,73.2724491 L19.2537042,93.3124491 L24.1737042,73.6724491 C24.1737042,73.1391131 24.1070382,72.6857843 23.9737042,72.3124491 C23.8403702,71.9391139 23.493707,71.7257827 22.9337042,71.6724491 L22.9337042,70.6724491 Z" id="5" transform="translate(19.433704, 86.672449) rotate(-112.000000) translate(-19.433704, -86.672449) "></path>
+				                </g>
+				            </g>
+				        </g>
+				    </g>
+				</svg>
+            </div>
+            <div class="data-water">
+	            <div class="wave"></div>
+            	There is
+            	<br>8%
+            	<br> water on this planet.
+            </div>
+
         </div>
 
     </div>
@@ -28,7 +70,6 @@
     background-position: center center;
     background-size: cover;
     color: white;
-    padding-top: 50px;
     text-align: center;
 }
 
@@ -45,6 +86,24 @@
     left: 20px;
 }
 
+.wrapper
+{
+	position: relative;
+	width: 1000px;
+	height: 1000px;
+	top: 50%;
+	left: 50%;
+	transform: translateX(-50%) rotate(0deg);
+	animation: wrapper-rotate 30s linear infinite ;
+}
+
+@keyframes wrapper-rotate
+{
+	0%	 {transform: translateX(-50%) rotate(0deg);}
+	100% {transform: translateX(-50%) rotate(360deg);}
+}
+
+
 .planet-tatooine
 {
     width: 300px;
@@ -52,16 +111,144 @@
     background-image: url(../../img/planet-Tatooine.png);
     background-size: cover;
     background-position: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+}
+
+.round-1
+{
+	width: 500px;	
+	height: 500px;
+	position: absolute;
+	border-radius: 100%;
+	border: 2px solid grey;
+	top: 50%;
+	left: 50%;
+    transform: translateX(-50%) translateY(-50%); 
+}
+
+.round-2
+{
+	width: 700px;	
+	height: 700px;
+	position: absolute;
+	border-radius: 100%;
+	border: 2px solid grey;
+	top: 50%;
+	left: 50%;
+	transform: translateX(-50%) translateY(-50%);	
+}
+
+.round-3
+{
+	width: 900px;	
+	height: 900px;
+	position: absolute;
+	border-radius: 100%;
+	border: 2px solid grey;
+	top: 50%;
+	left: 50%;
+	transform: translateX(-50%) translateY(-50%);		
+}
+
+.data-slider
+{
+	position: absolute;
+	top: 80px;
+	left: 515px;
+	animation: rotate-slider 30s linear infinite;
+}
+
+@keyframes rotate-slider
+{
+	0%	 {transform: rotate(0deg);}
+	100% {transform: rotate(-360deg);}
+}
+
+
+.data-resident
+{
+	position: absolute;
+	background-image: url(../../img/DataResidentTatooine.svg);
+	background-size: cover;
+	background-position: center;
+	width: 160px;
+	height: 160px;
+	top: 665px;
+	left: 50%;
+	transform: translateX(-50%) rotate(0deg);
+	text-align: center;
+	padding: 45px 0px;
+	animation: rotate-resident 30s linear infinite;
+}
+
+@keyframes rotate-resident
+{
+	0%	 {transform: translateX(-50%) rotate(0deg);}
+	100% {transform: translateX(-50%) rotate(-360deg);}
+}
+
+.data-chapter
+{
+	position: absolute;
+	width: 175px;
+	height: 175px;
+	top: 200px;
+	left: 820px;
+	animation: rotate-chapter 30s linear infinite;
+}
+
+@keyframes rotate-chapter
+{
+	0%	 {transform: rotate(0deg);}
+	100% {transform: rotate(-360deg);}
+}
+
+.data-chapter svg
+{
+	width: 100%;
+}
+
+.data-water
+{
+	width: 150px;
+	height: 150px;
+	border-radius: 100%;
+	border: 2px solid white;
+	position: absolute;
+	top: 500px;
+	left: 85px;
+	padding-top: 20px;
+	background-image: url(../../img/wave.png);
+	background-position: 0px 110px;
+	background-repeat: repeat-x;
+	-webkit-background-size: contain;
+	background-size: contain;
+	animation: waving 3s linear infinite, water-rotate 30s linear infinite;
+}
+
+@keyframes waving
+{
+	0% { background-position: 0px 110px; }
+	100% { background-position: 300px 110px; }
+}
+
+@keyframes water-rotate
+{
+	0%	 {transform: rotate(0deg);}
+	100% {transform: rotate(-360deg);}	
 }
 
 /*
 SLIDER 2 BETTER*/
 
 .alfa {
-    background-image: url(https://unsplash.it/500/500?random=1);
-    width: 500px;
-    height: 500px;
-    border: 20px solid #ffe81f;
+    background-image: url(../../img/slider_tatooine_real.jpg);
+    width: 150px;
+    height: 150px;
+    border: 2px solid #ffffff;
     border-radius: 100%;
     box-shadow: 0 0 1px #999, -2px 2px 3px rgba(0,0,0,.2);
     padding: 0;
@@ -71,8 +258,8 @@ SLIDER 2 BETTER*/
 }
 
 .bravo {
-    background-image: url(https://unsplash.it/500/500?random=2);
-    height: 500px;
+    background-image: url(../../img/slider_tatooine_film.png);
+    height: 150px; 
     resize: horizontal;
     position: absolute;
     top: 0; left: 0;
@@ -81,8 +268,9 @@ SLIDER 2 BETTER*/
     -moz-box-sizing: border-box;
     box-sizing: border-box;
 }
+
 .bravo:before {
-    content: '<->';
+    content: '-';
     background: rgba(0,0,0,.7);
     font-size: 18px;
     color: white;
